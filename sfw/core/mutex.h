36259dc875926328b9927d6cf9c1fd7d45387c3d
Relintai <relintai@relintai.net>
2026-02-28 09:50:13 +0100

    Backport new mutex from Pandemonium.

--- sfw/core/mutex.h
+++ sfw/core/mutex.h
@@ -11,49 +11,70 @@
 //--STRIP
 #include "core/error_list.h"
 #include "core/typedefs.h"
+
+#if !defined(NO_THREADS)
+
+#if !defined(_WIN64) && !defined(_WIN32)
+#include <pthread.h>
+#endif
+
+#endif
+
 //--STRIP
 
 #if !defined(NO_THREADS)
 
-#include <mutex>
+#if defined(_WIN64) || defined(_WIN32)
 
-template <class StdMutexT>
-class MutexImpl {
-	mutable StdMutexT mutex;
+class Mutex {
 	friend class MutexLock;
 
+	// void* because we don't want to include windows.h in this header.
+	mutable void *mutex;
+
 public:
-	_ALWAYS_INLINE_ void lock() const {
-		mutex.lock();
-	}
+	void lock() const;
+	void unlock() const;
+	Error try_lock() const;
 
-	_ALWAYS_INLINE_ void unlock() const {
-		mutex.unlock();
-	}
+	Mutex();
+	~Mutex();
+};
 
-	_ALWAYS_INLINE_ Error try_lock() const {
-		return mutex.try_lock() ? OK : ERR_BUSY;
-	}
+class BinaryMutex {
+	friend class MutexLock;
+
+	// void* because we don't want to include windows.h in this header.
+	mutable void *mutex;
+
+public:
+	void lock() const;
+	void unlock() const;
+	Error try_lock() const;
+
+	BinaryMutex();
+	~BinaryMutex();
 };
 
 // This is written this way instead of being a template to overcome a limitation of C++ pre-17
 // that would require MutexLock to be used like this: MutexLock<Mutex> lock;
 class MutexLock {
 	union {
-		std::recursive_mutex *recursive_mutex;
-		std::mutex *mutex;
+		Mutex *recursive_mutex;
+		BinaryMutex *mutex;
 	};
 	bool recursive;
 
 public:
-	_ALWAYS_INLINE_ explicit MutexLock(const MutexImpl<std::recursive_mutex> &p_mutex) :
-			recursive_mutex(&p_mutex.mutex),
-			recursive(true) {
+	_ALWAYS_INLINE_ explicit MutexLock(const Mutex &p_mutex) {
+		recursive_mutex = const_cast<Mutex *>(&p_mutex);
+		recursive = true;
+
 		recursive_mutex->lock();
 	}
-	_ALWAYS_INLINE_ explicit MutexLock(const MutexImpl<std::mutex> &p_mutex) :
-			mutex(&p_mutex.mutex),
-			recursive(false) {
+	_ALWAYS_INLINE_ explicit MutexLock(const BinaryMutex &p_mutex) {
+		mutex = const_cast<BinaryMutex *>(&p_mutex);
+		recursive = false;
 		mutex->lock();
 	}
 
@@ -66,11 +87,99 @@ public:
 	}
 };
 
-using Mutex = MutexImpl<std::recursive_mutex>; // Recursive, for general use
-using BinaryMutex = MutexImpl<std::mutex>; // Non-recursive, handle with care
+#else
 
-extern template class MutexImpl<std::recursive_mutex>;
-extern template class MutexImpl<std::mutex>;
+class Mutex {
+	friend class MutexLock;
+
+	mutable pthread_mutexattr_t attr;
+	mutable pthread_mutex_t mutex;
+
+public:
+	_ALWAYS_INLINE_ void lock() const {
+		pthread_mutex_lock(&mutex);
+	}
+
+	_ALWAYS_INLINE_ void unlock() const {
+		pthread_mutex_unlock(&mutex);
+	}
+
+	_ALWAYS_INLINE_ Error try_lock() const {
+		return (pthread_mutex_trylock(&mutex) == 0) ? OK : ERR_BUSY;
+	}
+
+	Mutex() {
+		pthread_mutexattr_init(&attr);
+		pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+		pthread_mutex_init(&mutex, &attr);
+	}
+
+	~Mutex() {
+		pthread_mutex_destroy(&mutex);
+	}
+};
+
+class BinaryMutex {
+	friend class MutexLock;
+
+	mutable pthread_mutexattr_t attr;
+	mutable pthread_mutex_t mutex;
+
+public:
+	_ALWAYS_INLINE_ void lock() const {
+		pthread_mutex_lock(&mutex);
+	}
+
+	_ALWAYS_INLINE_ void unlock() const {
+		pthread_mutex_unlock(&mutex);
+	}
+
+	_ALWAYS_INLINE_ Error try_lock() const {
+		return (pthread_mutex_trylock(&mutex) == 0) ? OK : ERR_BUSY;
+	}
+
+	BinaryMutex() {
+		pthread_mutexattr_init(&attr);
+		pthread_mutex_init(&mutex, &attr);
+	}
+
+	~BinaryMutex() {
+		pthread_mutex_destroy(&mutex);
+	}
+};
+
+// This is written this way instead of being a template to overcome a limitation of C++ pre-17
+// that would require MutexLock to be used like this: MutexLock<Mutex> lock;
+class MutexLock {
+	union {
+		Mutex *recursive_mutex;
+		BinaryMutex *mutex;
+	};
+	bool recursive;
+
+public:
+	_ALWAYS_INLINE_ explicit MutexLock(const Mutex &p_mutex) {
+		recursive_mutex = const_cast<Mutex *>(&p_mutex);
+		recursive = true;
+
+		recursive_mutex->lock();
+	}
+	_ALWAYS_INLINE_ explicit MutexLock(const BinaryMutex &p_mutex) {
+		mutex = const_cast<BinaryMutex *>(&p_mutex);
+		recursive = false;
+		mutex->lock();
+	}
+
+	_ALWAYS_INLINE_ ~MutexLock() {
+		if (recursive) {
+			recursive_mutex->unlock();
+		} else {
+			mutex->unlock();
+		}
+	}
+};
+
+#endif
 
 #else
 
--- sfwl/core/mutex.cpp
+++ sfwl/core/mutex.cpp
@@ -5,6 +5,13 @@
 
 //--STRIP
 #include "core/mutex.h"
+#include "core/memory.h"
+
+#if defined(_WIN64) || defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+
 //--STRIP
 
 static Mutex _global_mutex;
@@ -19,7 +26,118 @@ void _global_unlock() {
 
 #ifndef NO_THREADS
 
-template class MutexImpl<std::recursive_mutex>;
-template class MutexImpl<std::mutex>;
+#if defined(_WIN64) || defined(_WIN32)
+
+void Mutex::lock() const {
+#ifdef WINDOWS_USE_MUTEX
+	WaitForSingleObject((HANDLE)mutex, INFINITE);
+#else
+	EnterCriticalSection((CRITICAL_SECTION *)mutex);
+#endif
+}
+
+void Mutex::unlock() const {
+#ifdef WINDOWS_USE_MUTEX
+	ReleaseMutex((HANDLE)mutex);
+#else
+	LeaveCriticalSection((CRITICAL_SECTION *)mutex);
+#endif
+}
+
+Error Mutex::try_lock() const {
+#ifdef WINDOWS_USE_MUTEX
+	return (WaitForSingleObject((HANDLE)mutex, 0) == WAIT_TIMEOUT) ? ERR_BUSY : OK;
+#else
+
+	if (TryEnterCriticalSection((CRITICAL_SECTION *)mutex)) {
+		return OK;
+	} else {
+		return ERR_BUSY;
+	}
+#endif
+}
+
+Mutex::Mutex() {
+#ifdef WINDOWS_USE_MUTEX
+	mutex = CreateMutex(NULL, FALSE, NULL);
+#else
+
+	mutex = memnew(CRITICAL_SECTION);
+
+#ifdef UWP_ENABLED
+	InitializeCriticalSectionEx((CRITICAL_SECTION *)mutex, 0, 0);
+#else
+	InitializeCriticalSection((CRITICAL_SECTION *)mutex);
+#endif
+#endif
+}
+
+Mutex::~Mutex() {
+#ifdef WINDOWS_USE_MUTEX
+	CloseHandle((HANDLE)mutex);
+#else
+
+	DeleteCriticalSection((CRITICAL_SECTION *)mutex);
+
+	memdelete((CRITICAL_SECTION *)mutex);
+#endif
+}
+
+void BinaryMutex::lock() const {
+#ifdef WINDOWS_USE_MUTEX
+	WaitForSingleObject((HANDLE)mutex, INFINITE);
+#else
+	EnterCriticalSection((CRITICAL_SECTION *)mutex);
+#endif
+}
+
+void BinaryMutex::unlock() const {
+#ifdef WINDOWS_USE_MUTEX
+	ReleaseMutex((HANDLE)mutex);
+#else
+	LeaveCriticalSection((CRITICAL_SECTION *)mutex);
+#endif
+}
+
+Error BinaryMutex::try_lock() const {
+#ifdef WINDOWS_USE_MUTEX
+	return (WaitForSingleObject((HANDLE)mutex, 0) == WAIT_TIMEOUT) ? ERR_BUSY : OK;
+#else
+
+	if (TryEnterCriticalSection((CRITICAL_SECTION *)mutex)) {
+		return OK;
+	} else {
+		return ERR_BUSY;
+	}
+#endif
+}
+
+BinaryMutex::BinaryMutex() {
+#ifdef WINDOWS_USE_MUTEX
+	mutex = CreateMutex(NULL, FALSE, NULL);
+#else
+
+	mutex = memnew(CRITICAL_SECTION);
+
+#ifdef UWP_ENABLED
+	InitializeCriticalSectionEx((CRITICAL_SECTION *)mutex, 0, 0);
+#else
+	InitializeCriticalSection((CRITICAL_SECTION *)mutex);
+#endif
+#endif
+}
+
+BinaryMutex::~BinaryMutex() {
+#ifdef WINDOWS_USE_MUTEX
+	CloseHandle((HANDLE)mutex);
+#else
+
+	DeleteCriticalSection((CRITICAL_SECTION *)mutex);
+
+	memdelete((CRITICAL_SECTION *)mutex);
+#endif
+}
+
+#endif
 
 #endif
